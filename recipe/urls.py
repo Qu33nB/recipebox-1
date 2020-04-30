7496ddba39f303e80a02baf0346f48518a7663d3
Sean Bailey <sepabailey@gmail.com>
2020-04-30 16:16:13 -0400

    Recipe box page links to recipe and authors page. links work and added home link as well

---
+++ recipe/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from recipe import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('', views.author),
+    path('', views.recipe)
+    # path('admin/', admin.site.urls),
+]
---
+++ recipe/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+from recipe.models import Author, RecipeItem
+
+# Create your views here.
+
+
+def index(request):
+    data = RecipeItem.objects.all()
+    return render(request, 'index.html', {'data': data})
+
+
+def author(request):
+    data = RecipeItem.objects.all()
+    return render(request, 'author.html', {'data': data})
+
+
+def recipe(request):
+    data = RecipeItem.objects.all()
+    return render(request, 'recipe.html', {'data': data})
